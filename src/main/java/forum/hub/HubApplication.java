a78c125b417e400585f517f62e6dd653662f5165
Jaehoon-So <jhspacelover@naver.com>
2023-09-13 18:23:08 +0900

    add: 기본 유저 추가

--- src/main/java/forum/hub/HubApplication.java
+++ src/main/java/forum/hub/HubApplication.java
@@ -1,7 +1,9 @@
 package forum.hub;
 
+import forum.hub.domain.entity.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
